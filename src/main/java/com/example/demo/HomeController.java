5404bddf695769c189fff013cc4ca6ae6cc203f8
Nikita-Shirbhate <140882209+Nikita-Shirbhate@users.noreply.github.com>
2025-02-10 21:39:09 +0530

    Update HomeController.java

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,22 +1,14 @@
+package com.example.sendevops;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.ui.Model;
-
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "index";  // Return the name of the HTML file (no need to include .html)
-    }
-}
-
-@Controller
-public class HomeController {
-    @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("message", "Welcome To Sen DevOps");
-        return "home";
+        // Serve the index.html page from the static folder
+        return "index";
     }
 }
